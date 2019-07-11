827d406358c071995ca51fda929e5c397a9e8aae
Vel2million <Vel2million@users.noreply.github.com>
2019-07-11 17:52:59 +0700

    Update JIRA_upload_file.py
    little fix

--- JIRA_upload_file.py
+++ JIRA_upload_file.py
@@ -3,7 +3,7 @@ import base64
 
 if __name__ == "__main__":
     b64Val = base64.b64encode('__USER__:__PASS__'.encode('utf-8'))
-    uriAddAttachment = "https://jira.bnc.ca/rest/api/2/issue/{0}/attachments".format('DHO-2064')
+    uriAddAttachment = "https://jira.bnc.ca/rest/api/2/issue/{0}/attachments".format('__TICKET_NO__')
     
     files = {'file': open('{0}'.format('__FILE_LOCATION__'), 'rb')}
     #headers
